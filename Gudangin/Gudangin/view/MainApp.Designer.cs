604367f8920a427931b8f345a64d4aed234f2f28
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2025-01-20 22:18:23 +0700

    awalan

--- Gudangin/Gudangin/view/MainApp.Designer.cs
+++ Gudangin/Gudangin/view/MainApp.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1056, 606);
             this.Name = "MainApp";
             this.Text = "Gudangin";
             this.ResumeLayout(false);
